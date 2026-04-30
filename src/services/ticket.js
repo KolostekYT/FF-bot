fe7540e608676c62b4321f36f998842ccef94ee8
KolostekYT <78088920+KolostekYT@users.noreply.github.com>
2026-04-30 14:38:57 +0200

    Update ticket.js

--- src/services/ticket.js
+++ src/services/ticket.js
@@ -162,7 +162,7 @@ export async function createTicket(guild, member, categoryId, reason = 'No reaso
     
     const embed = createEmbed({
       title: `Ticket #${ticketNumber}`,
-      description: `${member.toString()}, thanks for creating a ticket!\n\n**Reason:** ${reason}\n**Priority:** ${priorityInfo.emoji} ${priorityInfo.label}`,
+      description: `${member.toString()}, Dziękujemy za stworzenie Aplikacji do FrostFang! (doślij jeszcze ss z godzin w grze na steam, oraz ss z fame w grze) \n\n**Reason:** ${reason}\n**Priority:** ${priorityInfo.emoji} ${priorityInfo.label}`,
       color: priorityInfo.color,
       fields: [
         { name: 'Status', value: '🟢 Open', inline: true },
